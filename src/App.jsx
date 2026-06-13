a649e52315b025d58b59f83327d88e0858f5b2ff
Andres Contreras Alvarez <andrescontrerasalvarez066@gmail.com>
2026-06-13 12:49:40 -0500

    Add flashcards information for country-capital quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,64 @@
+import { useState } from 'react';
 import './App.css';
+
+const cards = [
+  {
+    country: 'Japan',
+    capital: 'Tokyo',
+    region: 'Asia',
+    flag: 'https://flagcdn.com/w320/jp.png',
+  },
+  {
+    country: 'Brazil',
+    capital: 'Brasilia',
+    region: 'South America',
+    flag: 'https://flagcdn.com/w320/br.png',
+  },
+  {
+    country: 'Canada',
+    capital: 'Ottawa',
+    region: 'North America',
+    flag: 'https://flagcdn.com/w320/ca.png',
+  },
+  {
+    country: 'Kenya',
+    capital: 'Nairobi',
+    region: 'Africa',
+    flag: 'https://flagcdn.com/w320/ke.png',
+  },
+  {
+    country: 'France',
+    capital: 'Paris',
+    region: 'Europe',
+    flag: 'https://flagcdn.com/w320/fr.png',
+  },
+  {
+    country: 'Australia',
+    capital: 'Canberra',
+    region: 'Oceania',
+    flag: 'https://flagcdn.com/w320/au.png',
+  },
+  {
+    country: 'Mexico',
+    capital: 'Mexico City',
+    region: 'North America',
+    flag: 'https://flagcdn.com/w320/mx.png',
+  },
+  {
+    country: 'Egypt',
+    capital: 'Cairo',
+    region: 'Africa',
+    flag: 'https://flagcdn.com/w320/eg.png',
+  },
+];
 
 const App = () => {
 
+
   return (
-    <div className="App">
+    <main>
+    </main>
+  );
+};
 
-    </div>
-  )
-}
-
-export default App
+export default App;
